faa954de79a151217734207ab668db139e71446e
Sampath Tharanga <sampaththaranga13@gmail.com>
2024-04-16 12:53:46 +0530

    Variables and data types

--- 3-Variables.py
+++ 3-Variables.py
@@ -3,3 +3,43 @@ b = "Sampath Tharanga" #type String
 
 print(a)
 print(b)
+
+
+print("\n")
+# If you want to specify the data type of a variable, this can be done with casting.
+
+p = str(34)
+q = int(34)
+r = float(34)
+
+
+print(p) # Type of String
+print(q) # Type of int
+print(r) # Type of float
+
+
+print("\n")
+# Many Values to Multiple Variables
+
+x, y, z = "Apple", "Orange", "Mango"
+print(x)
+print(y)
+print(z)
+
+
+
+print("\n")
+# One Value to Multiple Variables
+s = t = u = "Sampath Tharanga"
+
+print(s)
+print(t)
+print(u)
+
+
+print("\n")
+
+
+d = 33;
+e = "Sampath"
+print(d, e)
---
+++ 4-DataTypes.py
@@ -0,0 +1,28 @@
+a = "Sampath Tharanga" #str
+print("str : ", a)
+print(type(a), "\n")
+
+b = 30 #int
+print("int :", b)
+print(type(b), "\n")
+
+c = 30.5 #float
+print("float :", c)
+
+d = 5j #complex
+print("complex :", d)
+
+e = ["Sampath", "Kumara", "Asela", "Namal"] #list
+print("list :", e)
+
+f = ("Apple", "Mango", "Orange") #tuple
+print("tuple :", f)
+
+g = range(6)
+print("range :", g)
+
+h = {"name" : "Sampath", "age" : 33}
+print("dict :", h)
+
+i = {"Apple", "Orange", "Mango"}
+print("set :", i)
