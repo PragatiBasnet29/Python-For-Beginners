7bed0c572262adfc4a4d42401b216c42ab2ad5eb
Sampath Tharanga <sampaththaranga13@gmail.com>
2024-04-16 12:06:31 +0530

    Configuration and basic
    Hello World program, Python version check and Variables

---
+++ 2-PythonVersionCheck.py
@@ -0,0 +1,13 @@
+
+#This is Comment
+
+
+"""
+This is a comment
+written in
+more than just one line
+"""
+
+import sys
+    
+print("Available Python Version is " + sys.version)
---
+++ 3-Variables.py
@@ -0,0 +1,5 @@
+a = 33 #type int
+b = "Sampath Tharanga" #type String
+
+print(a)
+print(b)
